a2062076f69367692e8e763e5c794c0ba024e546
2020b Bognár Kevin <98013419+b0BogKevin@users.noreply.github.com>
2024-02-23 09:40:27 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var func = function(item){
+    return Number(item) % 2 === 0;
+ }
+ 
+ function map(arr, somefunction){
+   if (typeof somefunction !== 'function') return 'given argument is not a function';
+   if (!arr.length) return 'array should contain only numbers';
+
+   var eredmeny = []
+   for (let i = 0; i < arr.length; i++) {
+    if (isNaN(arr[i])) {
+        return 'array should contain only numbers';
+    }
+    eredmeny.push(somefunction(arr[i]))
+   }
+   return eredmeny
+ }
